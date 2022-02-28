999e64a55a2da533f84374b8776e6f95a25428f5
sduby <supersduby@gmail.com>
2022-03-01 02:56:30 +0800

    initial

---
+++ filesync/go.mod
@@ -0,0 +1,14 @@
+module main
+
+go 1.17
+
+require github.com/wdvxdr1123/ZeroBot v1.4.1
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/tidwall/gjson v1.11.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
---
+++ filesync/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"sync"
+
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/driver"
+)
+
+var groupNameMap sync.Map
+
+const server = "https://tony.sduby.top/api/remoteDownload"
+
+func getGroupName(id int64, ctx *zero.Ctx) string {
+	if name, ok := groupNameMap.Load(id); ok {
+		return name.(string)
+	}
+
+	group := ctx.GetGroupInfo(id, true)
+	groupNameMap.Store(id, group.Name)
+	return group.Name
+}
+
+func callRemoteDownload(filename string, fileurl string, groupname string) {
+	data := map[string]string{
+		"FileName":  filename,
+		"FileUrl":   fileurl,
+		"GroupName": groupname,
+	}
+	jsonvalue, _ := json.Marshal(data)
+	log.Println(string(jsonvalue))
+	resp, err := http.Post(server, "application/json", bytes.NewBuffer(jsonvalue))
+	if err != nil {
+		log.Printf("error calling api %v", err)
+	}
+	defer resp.Body.Close()
+}
+
+func main() {
+	zero.OnNotice().Handle(func(ctx *zero.Ctx) {
+		file := ctx.Event.File
+		if ctx.Event.NoticeType != "group_upload" {
+			return
+		}
+
+		params := zero.Params{
+			"group_id": ctx.Event.GroupID,
+			"file_id":  file.ID,
+			"busid":    file.BusID,
+		}
+
+		resp := ctx.CallAction("get_group_file_url", params)
+		url := resp.Data.Get("url").String()
+		groupname := getGroupName(ctx.Event.GroupID, ctx)
+		filename := file.Name
+		log.Println(filename, url, groupname)
+		go callRemoteDownload(filename, url, groupname)
+	})
+
+        zero.OnCommand("hello", zero.SuperUserPermission).Handle(func(ctx *zero.Ctx) {
+                ctx.Send("world")
+        })
+
+	zero.RunAndBlock(zero.Config{
+		NickName:      []string{"bot"},
+		CommandPrefix: "/",
+		SuperUsers:    []string{"2081807694"},
+		Driver: []zero.Driver{
+			driver.NewWebSocketClient("ws://127.0.0.1:6700", "access_token"),
+		},
+	})
+}
